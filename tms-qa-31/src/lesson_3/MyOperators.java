6bd8af44d215fb947dc6767656b7a21d53a592a5
achosikha <softdevelopment@yahoo.com>
2025-06-03 20:31:56 +0400

    Added lesson 3 package on Operators for TMS group

---
+++ tms-qa-31/src/lesson_3/MyOperators.java
@@ -0,0 +1,11 @@
+package lesson_3;
+
+// IDE (Project) - GIT (Virtual project)
+// Document ADDED Добавление документа
+// Commit Точка сохранения
+// Changes applied Изменения внесены - отражены
+
+// $ git add .
+// $ git commit -am "SOME MESSAGE"
+public class MyOperators {
+}
